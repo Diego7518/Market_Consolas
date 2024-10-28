e8262b9d1b8f0b509b25aed37510b65fdee7c811
Diego7518 <168736093+Diego7518@users.noreply.github.com>
2024-10-28 08:50:12 -0500

    Imagenes

---
+++ app.js
@@ -0,0 +1,30 @@
+const Consoles = [
+    {
+      title: 'Super nintendo',
+      price: '1.200.000',
+      img: '/img/Super nintendo.jpg',
+    },
+    {
+      title: 'Thriller',
+      artist: 'Michael',
+      img: 'https://m.media-amazon.com/images/I/71wntRXPjcL._SY200_QL15_.jpg',
+    },
+    {
+      title: 'Danza Kuduro',
+      artist: 'Don Omar',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnUSULuFkqZSSJtVxTM2rIRlz-1h7cZ3ZMTg&s',
+      song: '/audio/y2mate.com - Don Omar  Danza Kuduro Lyrics ft Lucenzo.mp3'
+    },
+    {
+      title: 'Get down',
+      artist: 'Backstreet boys',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHYem5tZKNTY9Hiq9P3LPJnZu9TR7Hg6f9lg&s',
+      song: '/audio/y2mate.com - Backstreet Boys  Get Down Youre The One For Me Official HD Video.mp3'
+    },
+    {
+      title: 'Bye-Bye-Bye',
+      artist: 'Nsync',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPzcEnOfzv_kPOPme-bKJHaVHLzQc1xO39IQ&s',
+      song: '/audio/y2mate.com - NSYNC  Bye Bye Bye Lyrics Deadpool 3 Soundtrack.mp3'
+    }
+  ];
